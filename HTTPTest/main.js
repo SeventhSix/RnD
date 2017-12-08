87951de8a2afc2aeded2054ef781dde87f4c0790
SeventhSix <highgatekeeper@gmail.com>
2017-12-08 16:22:02 +0200

    ...

--- HTTPTest/main.js
+++ HTTPTest/main.js
@@ -9,23 +9,79 @@ Command line parameters: parseInt(process.argv.slice(2)[0]));
 */
 
 var net = require('net');
+var util = require('util');
+var sleep = require('system-sleep');
 
-function BuildHTTPGETRequest(path, hostAndPort) {
-    return 'GET ' + path + ' HTTP/1.1\r\nHost: ' + hostAndPort + '\r\nConnection: close\r\n\r\n';
+function GetTestSocket() {
+	let client = new net.Socket();
+	
+	client.on('data', function(data) {
+		console.log('# Received:\r\n' + data);
+		//client.destroy();
+	});
+
+	client.on('close', function() {
+		console.log('# Closed:');
+		client.destroy();
+	});
+
+	return client;
 }
 
-var client = new net.Socket();
+function Test1(ipAddress, portNumber, requestPath, hostHeader) {
+	let client = GetTestSocket();
 
-client.on('data', function(data) {
-	console.log('Received:\r\n' + data);
-	client.destroy();
-});
+	client.connect(portNumber, ipAddress, function() {
+		console.log('# Connected:');
 
-client.on('close', function() {
-	console.log('Connection closed');
-});
+		let httpRequest = 'GET ' + requestPath + ' HTTP/1.1\r\nHost: ' + hostHeader + '\r\nConnection: close\r\n\r\n';
+		client.write(httpRequest);
+		console.log('# Sent:\r\n' + httpRequest);
+	});
+}
 
-client.connect(8181, '10.250.45.142', function() {
-	console.log('Connected');
-	client.write(BuildHTTPGETRequest('/hawtio/login', 'cbblvfensqa01:8181'));
-});
+function Test2(ipAddress, portNumber, requestPath, hostHeader) {
+	let client = GetTestSocket();
+
+	client.connect(portNumber, ipAddress, function() {
+		console.log('# Connected:');
+
+		let httpRequest = 'GET ' + requestPath + ' HTTP/1.1\r\nHost: ' + hostHeader + '\r';
+		client.write(httpRequest);
+		console.log('# Sent:\r\n' + httpRequest);
+
+		sleep(3000);
+
+		httpRequest = '\nConnection: close\r\n\r\n';
+		client.write(httpRequest);
+		console.log('# Sent:\r\n' + httpRequest);
+	});
+}
+
+function Test3(ipAddress, portNumber, requestPath, hostHeader) {
+	let client = GetTestSocket();
+
+	client.connect(portNumber, ipAddress, function() {
+		console.log('# Connected:');
+
+		let httpRequest = 'GET ' + requestPath + ' HTTP/1.1\r\nHost: ' + hostHeader + '\r\nConnection: close\r\n';
+		client.write(httpRequest);
+		console.log('# Sent:\r\n' + httpRequest);
+
+		sleep(3000);
+
+		httpRequest = '\r\n';
+		client.write(httpRequest);
+		console.log('# Sent:\r\n' + httpRequest);
+	});
+}
+
+
+let ipAddress = '10.250.45.142';
+let portNumber = 8181;
+let requestPath = '/hawtio/login';
+let hostHeader = 'cbblvfensqa01:8181';
+
+Test3(ipAddress, portNumber, requestPath, hostHeader);
+
+sleep(1500);
