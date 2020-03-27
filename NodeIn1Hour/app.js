7897a4b5153f9ffbddf2bfb57a47c8c5be259de5
SeventhSix <highgatekeeper@gmail.com>
2020-03-27 03:48:52 +0200

    save

--- NodeIn1Hour/app.js
+++ NodeIn1Hour/app.js
@@ -1,3 +1,5 @@
+var os = require('os');
+
 function sayHello(name, count) {
     console.log('Hello ' + name + ' : ' + count);
 }
@@ -6,11 +8,18 @@ function sayHelloRepeater(name) {
     sayHello(name, count);
     count = count + 1;
 
-    if(count < 10) {
+    if(count < 2) {
         setTimeout(sayHelloRepeater, 1500, name);
     }
 }
 
 global.console.log('Running ...');
+console.log('==============================================================');
+console.log('');
+console.log(os.cpus());
+console.log('');
+console.log('==============================================================');
+console.log('');
+
 let count = 0;
 sayHelloRepeater('Generic message ...');
